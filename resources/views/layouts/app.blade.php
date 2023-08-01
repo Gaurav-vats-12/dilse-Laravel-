017a239db43cb23d8eb019eaf3faf79ac4d53f7d
Bheem Singh <121233593+Bheem-singh-mexoticaita@users.noreply.github.com>
2023-08-01 15:33:25 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @yield('meta')
+    @yield('meta cnmzxc')
         <title>{{ setting('site_title', 'LaraStarter') }}</title>
     @include('layouts.partials.header_style')
 </head>
