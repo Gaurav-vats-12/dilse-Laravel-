b4964898d6edffb100b7f8870d7c8b13f2815c11
Shaurya Dogra <shaurya.dograexoticait@gmail.com>
2023-08-02 11:26:18 +0530

    contact page done

---
+++ resources/views/emails/contact_notification.blade.php
@@ -0,0 +1,5 @@
+<h1>New Contact Form Submission</h1>
+<p>Name: {{ $contactData['first_name'] }} {{ $contactData['last_name'] }}</p>
+<p>Email: {{ $contactData['email'] }}</p>
+<p>Phone: {{ $contactData['phone'] }}</p>
+<p>Message: {{ $contactData['message'] }}</p>
