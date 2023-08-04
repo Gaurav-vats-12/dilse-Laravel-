052b3a06632c6fb712c9571ada788c7dd57f5b90
Bheem Singh <121233593+Bheem-singh-mexoticaita@users.noreply.github.com>
2023-08-04 14:06:35 +0530

    Update singleblogdetails.blade.php

--- resources/views/Pages/Blog/singleblogdetails.blade.php
+++ resources/views/Pages/Blog/singleblogdetails.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('title', 'Blog')
-
 @section('frontcontent')
 <section class="inner_bannner bg_style" style="background-image: url('{{asset('frontend/img/about_banner-image.png') }}')">
         <div class="about_banner_section">
@@ -18,5 +17,4 @@
           {!!$blog->blog_content !!}
         </div>
       </section>
-
 @endsection
