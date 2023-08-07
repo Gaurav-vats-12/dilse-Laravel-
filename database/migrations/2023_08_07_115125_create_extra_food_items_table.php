e279d28a6c96d2cfb7cda031c1a2d0621b4ce0fe
Bheem Singh <121233593+Bheem-singh-mexoticaita@users.noreply.github.com>
2023-08-07 18:38:17 +0530

    Update 2023_08_07_115125_create_extra_food_items_table.php

--- database/migrations/2023_08_07_115125_create_extra_food_items_table.php
+++ database/migrations/2023_08_07_115125_create_extra_food_items_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('extra_food_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('food_item_id')->constrained()->on('food_items')->nullable()->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('extra_item_id')->constrained()->on('extra_items')->nullable()->onUpdate('cascade')->onDelete('cascade');
+            $table->bigInteger('food_item_id')->unsigned();
+            $table->bigInteger('extra_item_id')->unsigned();
+            $table->foreign('food_item_id')->references('id')->on('food_items')->onDelete('cascade');
+            $table->foreign('extra_item_id')->references('id')->on('extra_items')->onDelete('cascade');
             $table->timestamps();
         });
     }
