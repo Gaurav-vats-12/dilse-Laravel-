66f3dcd44a2814b2a0fcf2441502158ee072f430
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-08-08 16:31:10 +0530

    Update Code

--- app/Modules/Users/routes/user.php
+++ app/Modules/Users/routes/user.php
@@ -1,14 +1,14 @@
 <?php
 
 use App\Modules\Users\Http\Controllers\ProfileController;
-use App\Http\Controllers\User\UserControoller;
+use App\Http\Controllers\User\UserController;
 
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('user')->name('user.')->group(function(){
     Route::middleware(['web', 'user.auth', 'user.verified'])->group(function(){
-        Route::get('/dashboard', [UserControoller::class, 'dashboard'])->name('dashboard');
+        Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
     });
 });
 
