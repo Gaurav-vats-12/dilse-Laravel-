d4884b04060926352f43f20f717ec544fa860362
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-08-08 17:28:39 +0530

    Update 2023_02_21_000001_create_users_table.php

--- app/Modules/Users/Database/migrations/2023_02_21_000001_create_users_table.php
+++ app/Modules/Users/Database/migrations/2023_02_21_000001_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->boolean('status')->default(1);
+
             $table->timestamps();
         });
     }
