da70cf3143316a96b3fa870f755240ac65bfdbd6
Exotica It Solution <78149557+exoticaitsolutions@users.noreply.github.com>
2023-08-08 16:02:22 +0530

    Added User Profile

---
+++ resources/views/user/auth/login.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+@section('title', 'User Login')
+@section('frontcontent')
+<div class="login_wrapper">
+      <section class="about_se about_left_image py_8">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-6">
+              <div class="about_img about_sign_img">
+                <img src="{{asset('frontend/img/signin.png')}}" alt="">
+              </div>
+            </div>
+            <div class="col-md-6">
+            <form method="POST" action="{{ route('user.login') }}"  class="contact_form">  @csrf
+                  <div class="tittle_heading">
+                    <h2>SIGN IN TO YOUR <span>ACCOUNT</span></h2>  </div>
+                    @if (session()->has('success'))<p>{!! session()->get('success') !!} </p>@endif
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="custn_input">
+                      <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')"  autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2 taxt-danger" />
+                      </div>
+                    </div>
+                     <div class="col-md-12">
+                     <div class="custn_input">
+                     <x-input-label for="password" :value="__('Password')" />
+                    <x-text-input id="password" class="block mt-1 w-full" type="password"  name="password"  autocomplete="current-password" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                      </div>
+                    </div>
+                    <div class="forget_passcode">
+                     <div class="under_section-form">
+                     <input id="remember_me" type="checkbox"  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                        <label for="remember_me" class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</label>
+                    </div>
+                        @if (Route::has('user.password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('user.password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+                    </div>
+
+                   <div class="contact_form_btn">
+                    <button class="theme_btn">  {{ __('Log in') }}</button>
+                  </div>
+                  <p>Don’t have an account?<span><a href="{{ route('user.register') }}">sign-up</a></span></p>
+
+                </form>
+              </div>
+          </div>
+        </div>
+      </section>
+    </div>
+@endsection
---
+++ resources/views/user/auth/reset-password.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+@section('title', 'User Reset  password')
+@section('frontcontent')
+<div class="login_wrapper">
+      <section class="about_se about_left_image py_8">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-6">
+              <div class="about_img about_sign_img">
+                <img src="{{asset('frontend/img/forgetpassword.png')}}" alt="">
+              </div>
+            </div>
+            <div class="col-md-6">
+                <form method="POST" action="{{ route('user.password.store') }}" class="contact_form"> @csrf
+                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                  <div class="tittle_heading">
+                    <h2>RESET YOUR <span>PASSWORD</span></h2>
+
+                <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="custn_input">
+                      <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="block mt-1 w-full"  readonly type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                      </div>
+                      <div class="custn_input">
+                      <x-input-label for="password" :value="__('Password')" />
+                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password"  autocomplete="new-password" />
+                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                      </div>
+                      <div class="custn_input">
+                      <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                        <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"  name="password_confirmation"  autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                      </div>
+                    </div>
+
+               </div>
+                  <div class="contact_form_btn">
+                    <button class="theme_btn">    {{ __('Reset Password') }}</button>
+                  </div>
+                  </form>
+              </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+
+@endsection
