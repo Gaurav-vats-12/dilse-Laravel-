ed01392f10f754485969db2c382076c67726e8cb
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-08-09 15:13:31 +0530

    Update Code

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -9,12 +9,12 @@ class MenuController extends Controller
     if ($request->ajax()) {
         $slug = $request->slug;
         $menu_id = Menu::where('menu_slug',$slug)->first()->id;
-        $FoodItem = FoodItem::where('menu_id',$menu_id)->paginate(3);
+        $FoodItem = FoodItem::where('menu_id',$menu_id)->paginate(12);
         return view('ajax.menufooditems',compact('FoodItem','slug'));
     }else{
         $slug = 'appetizers';
         $menu_id = Menu::where('menu_slug',$slug)->first()->id;
-        $FoodItem = FoodItem::where('menu_id',$menu_id)->paginate(3);
+        $FoodItem = FoodItem::where('menu_id',$menu_id)->paginate(12);
         return view('Pages.menu',compact('FoodItem','slug'));
     }
 
