cec79e8c22aa920afca1e272a03f941fbbc43d6a
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-08-24 16:54:45 +0530

    Fixed  Bugs

--- resources/views/admin/partials/navigation.blade.php
+++ resources/views/admin/partials/navigation.blade.php
@@ -6,7 +6,6 @@
       </li>
     </ul>
     <ul class="navbar-nav ml-auto">
-
       <li class="nav-item dropdown">
             <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">{{ Auth::guard('admin')->check() ? Auth::guard('admin')->user()->name : '' }}</a>
             <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow" style="left: 0px; right: inherit;">
