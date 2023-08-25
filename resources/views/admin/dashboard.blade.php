bf63986f1d9788738a8204a747bd90b260a604af
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-08-25 18:34:20 +0530

    Minor Bug's Fixed

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -89,7 +89,7 @@
               <div class="icon">
                 <i class="ion ion-pie-graph"></i>
               </div>
-              <a href="{{ route('admin.manage-gallery.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ route('admin.booking.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
         </div>
