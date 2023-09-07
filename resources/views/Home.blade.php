5862e3b2919a017a32b922f5b5cbc20fe3976880
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-09-07 10:29:17 +0530

    Update Home.blade.php

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -6,7 +6,7 @@
 <section class="video_dilse-tw">
   <div class="video_dilse_play-tw">
     <video id="home_banner_vd" width="" height="" autoplay loop muted>
-      <source src="{{asset('frontend/vedio/DilSe Home.mp4#t=13.8')}}" type="video/mp4" />
+      <source src="{{asset('frontend/vedio/DilSe Home.mp4')}}" type="video/mp4" />
     </video>
     <div class="home-slider-main-tw">
       <div class="container">
