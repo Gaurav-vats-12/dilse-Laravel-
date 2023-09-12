c448575f3a92bb451f085cd752c642d8550268ee
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-09-12 12:56:06 +0530

    Update Code

--- resources/views/layouts/partials/navigate.blade.php
+++ resources/views/layouts/partials/navigate.blade.php
@@ -44,8 +44,7 @@
                                         src="{{asset('frontend/img/blogto.png') }}" alt=""/></a>
                         </li>
 
-                        <li><select class="select_location" name="select_location" id="select_location" ajax_value ="{{ route('cart.update_location')}}">
-
+                        <li><select class="select_location" name="select_location" id="select_location" ajax_value ="{{ route('cart.update_details')}}" location_Type="location">
                         <option value="Toronto" {{ session('update_location') =='Toronto'? 'selected' : '' }}>Toronto</option>
                                 <option value="Brampton" {{ session('update_location') =='Brampton'? 'selected' : '' }}>Brampton</option>
                             </select></li>
