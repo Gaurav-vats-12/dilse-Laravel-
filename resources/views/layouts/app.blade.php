32ad9659ad33dbb0ea4deadbe105dfe4d31e8f78
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-09-13 18:35:13 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <title>{{ setting('site_title', 'Dilse') }} | @yield('title')</title>
         <link rel="icon" type="image/x-icon" href="{{ setting('favicon') != null ? url('/storage/site/Favicon/'.setting('favicon').'') : url('frontend/img/fav.png') }}">
     @include('layouts.partials.header_style')
+    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/ef3584d822e3060c8cdb4139f/e030a1a2fb366d50e1638334e.js");</script>
 </head>
 <body>
 <a id="button"></a>
