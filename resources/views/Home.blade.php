f99991ecf244cfa92fddc7be943b215954b54327
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-09-14 17:28:42 +0530

    Update Code  and Push Code

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -400,18 +400,6 @@ We provide dine-in, take-out, delivery, and catering service for popular Indian 
     </form>
     </div>
     <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script></div>
-
-        <!-- <form class="sub_scribe_form" id="emailSubscribeForm" method="POST">
-          <input type="hidden" name="email_action_url" id="email_action_url" value="{{ route('emailSubscription') }}">
-
-          <div class="custn_input">
-            <input type="text" placeholder="Enter your email" id="email_address" name="email_address" />
-            <span id="email_address-error" class="text-danger error"></span>
-          </div>
-          <div class="sub_scribe_form_btn">
-            <button class="theme_btn btn-txt">Subscribe Now</button>
-          </div>
-        </form> -->
       </div>
     </div>
   </div>
