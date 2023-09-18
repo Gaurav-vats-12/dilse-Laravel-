015b9b6e276198f8033ab595e2e9681532c27e3b
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-09-18 18:40:54 +0530

    Update
    Update

--- resources/views/ajax/menufooditems.blade.php
+++ resources/views/ajax/menufooditems.blade.php
@@ -26,7 +26,9 @@
                         @endforeach
 
                         <div class="CustomPagination"  id="menu_items">
-                            {!! $FoodItem->links() !!}
+                            <!-- {!! $FoodItem->links() !!} -->
+                            {!! $FoodItem->render() !!}
+
                         </div>
                     @else
                     <h4>No Food Item  Found</h4>
