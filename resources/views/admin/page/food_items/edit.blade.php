32ef956fa0a7381de924cd9f51497929b1e815b2
Bheem Singh <mail.exoticaitsolutions@gmail.com>
2023-09-22 10:24:49 +0530

    Update the Code In  Food Item Controller

--- resources/views/admin/page/food_items/edit.blade.php
+++ resources/views/admin/page/food_items/edit.blade.php
@@ -3,12 +3,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Create Food Items</h1>
+            <h1 class="m-0">Edit Food Items</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-            <li class="breadcrumb-item active">Create Food Items</li>
+            <li class="breadcrumb-item active">Edit Food Items</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
